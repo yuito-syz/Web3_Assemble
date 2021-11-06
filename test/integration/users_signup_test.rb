359aec24e4b3d73dd8b63baae7cadeab91df7025
yuito-syz <yuito_sy1122@icloud.com>
2021-11-06 13:49:07 +0000

    Login - Basic login mechanism

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -22,5 +23,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in?
   end
 end
