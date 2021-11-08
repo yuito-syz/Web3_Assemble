b613d8af87a7a830075f563240cda695c3c6fe31
yuito-syz <yuito_sy1122@icloud.com>
2021-11-08 03:28:53 +0000

    Send account activation email.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,9 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+  host = 'https://us-east-2.console.aws.amazon.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
