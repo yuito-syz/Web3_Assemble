68e2acd485c06dfb1e5d5c3c7f5ddf55cb951aee
yuito-syz <yuito_sy1122@icloud.com>
2021-12-15 19:33:49 +0900

    Add Like Function

--- .history/app/models/micropost_20211115221617.rb
+++ .history/app/models/micropost_20211115221617.rb
@@ -2,7 +2,6 @@ class Micropost < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_many :comments, dependent: :destroy
-  has_many :likes
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
