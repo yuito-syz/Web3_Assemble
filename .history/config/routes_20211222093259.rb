3bec45c9388d171f481a16c878f9c72ebe92b8a1
yuito-syz <yuito_sy1122@icloud.com>
2021-12-22 09:52:47 +0900

    Add a button to app/views/shared/_feed.html.erb

--- .history/config/routes_20211222093259.rb
+++ .history/config/routes_20211222093259.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
+  resources :post
   root 'static_pages#home'
-  get  '/help',    => 'static_pages#help'
-  get  '/about',   => 'static_pages#about'
-  get  '/contact', => 'static_pages#contact'
-  get  '/signup',  => 'users#new'
-  get    '/login',   => 'sessions#new'
-  post   '/login',   => 'sessions#create'
-  delete '/logout',  => 'sessions#destroy'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   get 'comments/:id/destroy' => 'comments#destroy'
   post 'comments/:id/create' => 'comments#create'
+  post "likes/:micropost_id/create", to: "likes#create", constraints: {micropost_id: /\d+/}, as: :likes_create
+  post "likes/:micropost_id/delete", to: "likes#delete", constraints: {micropost_id: /\d+/}, as: :likes_delete
   
   resources :users do
+    get :search, on: :collection
     member do
       get :following, :followers
     end
@@ -18,6 +22,7 @@ Rails.application.routes.draw do
       resources :comments, only: [:create]
     end
   end
+  
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
