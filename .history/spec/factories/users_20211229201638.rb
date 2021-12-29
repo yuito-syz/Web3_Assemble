b94e155fd93c5c104425dde45fe64acf0a913130
yuito-syz <yuito_sy1122@icloud.com>
2021-12-29 20:20:00 +0900

    Add rspec_RTch.6 #125

--- .history/spec/factories/users_20211229201638.rb
+++ .history/spec/factories/users_20211229201638.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
     factory :user do
       name { "TestUser" }
       sequence(:email) { |n| "test#{n}@example.com" }
-      password { 'foobar' }
-      password_confirmation { 'foobar' }
+      password { 'password' }
+      password_confirmation { 'password' }
     end
 end
