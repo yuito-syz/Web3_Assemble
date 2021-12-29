6ebd8b4426e94d5aa7d9a20224c90f15f4652b39
yuito-syz <yuito_sy1122@icloud.com>
2021-12-29 22:04:29 +0900

    Need to fix about rspec_RTch.7 #126

--- .history/spec/requests/users_request_spec_20211229210748.rb
+++ .history/spec/requests/users_request_spec_20211229210748.rb
@@ -9,7 +9,7 @@ RSpec.describe "Users", type: :request do
           post users_path, params: { user: user } # <= paramsの中身はハッシュの形
         end.to change(User, :count).by(1)
         expect(response).to have_http_status(302)
-        expect(response).to redirect_to user_path(User.last)
+        expect(response).to redirect_to users_path
       end
     end
   end
