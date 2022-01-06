7e8e7dfae9cddf32d406a995014339e4af1e4f7e
yuito-syz <yuito_sy1122@icloud.com>
2022-01-06 20:46:17 +0900

    In progress, rspec_RTch.8, #127

--- spec/factories/word_of_mouths.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :word_of_mouth do
-    
-  end
-end
---
+++ spec/requests/sessions_request_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "Sessions", type: :request do
+  let(:user) {FactoryBot.create(:user)}
+
+  describe "delete /logout" do
+    before do
+      post login_path, params: { session: {
+        email: user.email,
+        password: user.password,
+      } }
+    end
+
+    it 'redirects to root_path' do
+      delete logout_path
+      aggregate_failures do
+        expect(response).to redirect_to root_path
+        expect(is_logged_in?).to be_falsy
+      end
+    end
+  end
+end
