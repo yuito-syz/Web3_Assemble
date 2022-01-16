68e7b53c2dfea95fa27e47e4fef1090b16a75c6f
yuito-syz <yuito_sy1122@icloud.com>
2022-01-17 07:47:22 +0900

    Delete require_relative 'boot' in config/application.rb

--- .history/config/application_20220117074622.rb
+++ .history/config/application_20220117074622.rb
@@ -1,5 +1,3 @@
-require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -18,5 +16,14 @@ module SampleApp
     
     # 認証トークンをremoteフォームに埋め込む
     config.action_view.embed_authenticity_token_in_remote_forms = true
+
+    config.generators do |generator|
+      generator.test_framework :rspec,
+                       fixtures: true,
+               controller_specs: true,
+                   helper_specs: false,
+                  routing_specs: false
+      generator.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
