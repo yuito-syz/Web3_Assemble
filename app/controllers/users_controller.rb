58e9b52523d4e9817c70284febb959cbdcf596f9
yuito-syz <yuito_sy1122@icloud.com>
2022-01-23 20:14:46 +0900

    Usable API and token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,13 +14,6 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
-    if @user.save
-      @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
-      redirect_to root_url
-    else
-      render 'new'
-    end
   end
   
   def edit
@@ -67,11 +60,6 @@ class UsersController < ApplicationController
 
   
   private
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
 
     # 正しいユーザーかどうか確認
     def correct_user
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,1 +1,9 @@
-# APIモードはここのmiddlewareで設定を行う。
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'localhost:8080'
+      resource '*',
+      headers: :any,
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+      methods: [:get, :post, :options, :delete, :put, :patch, :head]
+    end
+end
