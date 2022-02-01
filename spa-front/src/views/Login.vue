4151750b2923cc1331e6bdc1c522641f50fa68bc
yuito-syz <yuito_sy1122@icloud.com>
2022-02-01 12:40:12 +0900

    Fixed Vue.js#262

--- spa-front/src/views/Login.vue
+++ spa-front/src/views/Login.vue
@@ -1,10 +1,31 @@
+# src/views/Login.vue
+
 <template>
   <div class="mt-16 px-16 mx-auto xl:max-w-3xl">
     <h2 class="text-center text-4xl text-indigo-900 font-display font-semibold lg:text-left xl:text-5xl
       xl:text-bold">
-      Log in
+      Login
     </h2>
-    <div class="mt-12">
+    <div v-if="isTwoFactorAuth" class="mt-12">
+      <div class="mt-8">
+        <div class="flex justify-between items-center">
+          <div class="text-sm font-bold text-gray-700 tracking-wide">
+            OtpCode
+          </div>
+        </div>
+        <input class="w-full text-lg py-2 border-b border-gray-300 focus:outline-none focus:border-indigo-500" v-model="otp_code" type="password" placeholder="Enter your OtpCode">
+      </div>
+      <div class="mt-10">
+        <button class="bg-indigo-500 text-gray-100 p-4 w-full rounded-full tracking-wide
+                       font-semibold font-display focus:outline-none focus:shadow-outline
+                       hover:bg-indigo-600 shadow-lg"
+                @click="handleLogin"
+        >
+          Login
+        </button>
+      </div>
+    </div>
+    <div v-else class="mt-12">
       <div class="mt-8">
         <div class="flex justify-between items-center">
           <div class="text-sm font-bold text-gray-700 tracking-wide">
@@ -27,43 +48,54 @@
                        hover:bg-indigo-600 shadow-lg"
                 @click="handleLogin"
         >
-          Log In
+          Login
         </button>
       </div>
     </div>
   </div>
 </template>
-  
+
 <script lang="ts">
-  import { defineComponent, reactive, toRefs } from 'vue'
-  import { login } from '@/api/auth'
-  import router from '@/router'
-  
-  export default defineComponent({
-    name: 'Home',
-    setup () {
-      const formData = reactive({
-        email: '',
-        password: ''
-      })
-  
-      return {
-        ...toRefs(formData),
-        handleLogin: async () => {
-          await login(formData.email, formData.password)
-            .then((res) => {
-              if (res?.status === 200) {
-                router.push('/posts') 
-              } else {
-                alert('メールアドレスかパスワードが間違っています。')
-              }
-            })
-            .catch(() => {
-              alert('ログインに失敗しました。')
-            })
-        }
-      }
+/* eslint-disable @typescript-eslint/camelcase */
+
+import { defineComponent, reactive, ref, toRefs } from 'vue'
+import { login } from '@/api/auth'
+import router from '@/router'
+
+export default defineComponent({
+  name: 'Home',
+  setup () {
+    const formData = reactive({
+      email: '',
+      password: '',
+      otp_code: ''
+    })
+
+    const isTwoFactorAuth = ref(false)
+
+    const handleLogin = async () => {
+      await login(formData.email, formData.password, formData.otp_code)
+        .then((res) => {
+          if (res?.data.two_factor_auth) {
+            isTwoFactorAuth.value = true
+          } else {
+            if (res?.status === 200) {
+              router.push('/')
+            } else {
+              alert('メールアドレスかパスワードが間違っています。')
+            }
+          }
+        })
+        .catch(() => {
+          alert('ログインに失敗しました。')
+        })
     }
-  })
+
+    return {
+      ...toRefs(formData),
+      isTwoFactorAuth,
+      handleLogin
+    }
+  }
+})
 </script>
-  
