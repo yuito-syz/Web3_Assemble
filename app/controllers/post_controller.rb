668dc1233341956143afd74e4a260ccf2dd1d7f8
yuito-syz <yuito_sy1122@icloud.com>
2022-02-04 17:08:36 +0900

    Deleted unused file & code #219

--- app/controllers/post_controller.rb
+++
@@ -1,9 +0,0 @@
-class PostController < ApplicationController
-    def index
-        @posts = Post.eager_load(:user).with_attached_icatch
-      end
-    
-    def post_params
-        params.permit(:title, :body, :icatch).merge(user: current_user)
-    end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -14,21 +14,4 @@ Rails.application.routes.draw do
       resource :two_factor_auth, only: [:show, :create]
     end
   end
-
-  resources :word_of_mouths
-  root 'static_pages#home'
-  get  '/help',    to: 'static_pages#help'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
-  resources :likes, only: [:create, :destroy]
-  resources :users do
-    get :search, on: :collection
-    member do
-      get :following, :followers
-    end
-    resources :microposts, only: %i[index show create] do
-    end
-  end
-  resources :microposts,          only: [:create, :destroy]
-  resources :relationships,       only: [:create, :destroy]
 end
