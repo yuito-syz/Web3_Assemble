d136881f8446191f07d56ac28b8dd5262e0f2136
yuito-syz <yuito_sy1122@icloud.com>
2022-02-16 21:40:57 +0900

    initialization#340

---
+++ frontend/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    index.vue
+  </div>
+</template>
+
+<script>
+export default {
+  layout ({ store }) {
+    return store.state.loggedIn ? 'default' : 'welcome'
+  }
+}
+</script>
