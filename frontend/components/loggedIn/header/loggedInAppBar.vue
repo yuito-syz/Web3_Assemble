f6d9243142386183c16cf5e709bcb6efc6a42886
yuito-syz <yuito_sy1122@icloud.com>
2022-02-27 18:34:02 +0900

    Add some pages

---
+++ frontend/components/loggedIn/header/loggedInAppBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app-bar
+    app
+    dense
+    elevation="1"
+    clipped-left
+    color="white"
+  >
+    <nuxt-link
+      to="/"
+      class="text-decoration-none"
+    >
+      <app-logo />
+    </nuxt-link>
+
+    <app-title />
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+}
+</script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -7,7 +7,8 @@
 <script>
 export default {
   layout ({ store }) {
-    return store.state.loggedIn ? 'default' : 'welcome'
+    // return store.state.loggedIn ? 'default' : 'welcome'
+    return store.state.loggedIn ? 'loggedIn' : 'welcome'
   }
 }
 </script>
