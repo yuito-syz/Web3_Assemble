c0cbffb8ee7ade2e12ff841cb3e2a2dfa5365c44
yuito-syz <yuito_sy1122@icloud.com>
2022-03-03 11:08:45 +0900

    created_top_page_layout#365

--- frontend/plugins/myInject.js
+++ frontend/plugins/myInject.js
@@ -8,6 +8,19 @@ class MyInject {
         const title = this.app.i18n.t(jsonPath)
         return (typeof (title) === 'object') ? title.index : title
     }
+
+    // 日時フォーマット変換
+    format (date) {
+        const dateTimeFormat = new Intl.DateTimeFormat(
+          'ja', { dateStyle: 'medium', timeStyle: 'short' }
+        )
+        return dateTimeFormat.format(new Date(date))
+    }
+    
+    // プロジェクトリンク
+    projectLinkTo (id, name = 'project-id-dashboard') {
+        return { name, params: { id } }
+    }
 }
   
 export default ({ app }, inject) => {
