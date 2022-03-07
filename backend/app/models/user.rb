161b435e8d3ae82e42d9540a40c9fa866fce29ff
yuito-syz <yuito_sy1122@icloud.com>
2022-03-07 12:09:31 +0900

    add_email_custom_validation

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,8 +1,13 @@
+require "validator/email_validator"
+
 class User < ApplicationRecord
+    before_validation :downcase_email
     has_secure_password
 
     validates :name, presence: true,
                    length: { maximum: 30, allow_blank: true }
+    validates :email, presence: true,
+                   email: { allow_blank: true }
     VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
     validates :password, presence: true,
                         length: { minimum: 8 },
@@ -11,4 +16,20 @@ class User < ApplicationRecord
                             message: :invalid_password
                         },
                         allow_nil: true
+    class << self
+        def find_activated(email)
+        find_by(email: email, activated: true)
+        end
+    end
+
+    def email_activated?
+        users = User.where.not(id: id)
+        users.find_activated(email).present?
+    end
+
+    private
+    
+    def downcase_email
+      self.email.downcase! if email
+    end
 end
--- backend/config/application.rb
+++ backend/config/application.rb
@@ -23,6 +23,7 @@ module SampleApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.autoload_paths += %W(#{config.root}/lib/validator)
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
