6773aad5a0f32a2aead9051569c7ad8d40d88dde
yuito-syz <yuito_sy1122@icloud.com>
2022-03-07 15:00:53 +0900

    add_signup_form_components

--- frontend/pages/signup.vue
+++ frontend/pages/signup.vue
@@ -1,11 +1,37 @@
 <template>
-  <div>
-    signup.vue
-  </div>
+  <bef-login-form-card>
+    <template
+      #form-card-content
+    >
+      <v-form
+        v-model="isValid"
+      >
+        <user-form-name />
+        <user-form-email />
+        <user-form-password />
+
+        <v-btn
+          :disabled="!isValid"
+          block
+          color="myblue"
+          class="white--text"
+        >
+          登録する
+        </v-btn>
+
+      </v-form>
+    </template>
+  </bef-login-form-card>
 </template>
 
 <script>
 export default {
-  layout: 'beforeLogin'
+  layout: 'beforeLogin',
+  
+  data () {
+    return {
+      isValid: false
+    }
+  }
 }
 </script>
