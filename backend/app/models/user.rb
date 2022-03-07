7dfb1ab06cbd09c4b0131e0bee025232c4951ac2
yuito-syz <yuito_sy1122@icloud.com>
2022-03-07 11:24:15 +0900

    Add UserModel_validation#338

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    validates :name, presence: true,
+                   length: { maximum: 30, allow_blank: true }
+    VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
+    validates :password, presence: true,
+                        length: { minimum: 8 },
+                        format: {
+                            with: VALID_PASSWORD_REGEX
+                        },
+                        allow_nil: true
 end
