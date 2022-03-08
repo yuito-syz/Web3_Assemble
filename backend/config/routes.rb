ef67918cc1e6f72e24ded612f8664dd6af4ba646
yuito-syz <yuito_sy1122@icloud.com>
2022-03-08 15:34:05 +0900

    add_user_token_controller.rb

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
     namespace :api do
         namespace :v1 do
-           resources :users, only:[] do
-               get :current_user, action: :show, on: :collection
-           end
+            resources :users, only:[] do
+                get :current_user, action: :show, on: :collection
+            end
+
+            resources :user_token, only: [:create] do
+                delete :destroy, on: :collection
+            end
         end
     end
 end
