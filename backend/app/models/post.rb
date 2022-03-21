98bf721a9e84c541466923c772dd2bc2d14c83a6
yuito-syz <yuito_sy1122@icloud.com>
2022-03-21 17:43:50 +0900

    add_draft_post_backend#358

--- backend/app/models/post.rb
+++ backend/app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
     belongs_to :user
     has_many :comments, dependent: :destroy
     has_many :likes, dependent: :destroy
+    enum status: { published: 0, draft: 1 }
 
     def liked_by?(user)
         likes.where(user_id: user.id).exists?
